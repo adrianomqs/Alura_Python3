188f162e90bc75d61889aca2e04da9b7371c5298
Adriano Marques <adriano.mqs@gmail.com>
2022-06-21 20:51:34 -0300

    Organização

---
+++ Main.py
@@ -0,0 +1,20 @@
+import adivinhacao
+import forca
+
+print("")
+print("*********************************")
+print("Bem vindo á Central de jogos!")
+print("*********************************")
+
+while True:
+    jogo = int(input('Escolha seu jogo. [1] Adivinhação ou [2] Forca?: '))
+
+    if jogo == 1:
+        # Adivinhação
+        adivinhacao.start()
+    elif jogo == 2:
+        # Forca
+        forca.start()
+    else:
+        # Não encontrado
+        print( 'Jogo não encontrado!')
